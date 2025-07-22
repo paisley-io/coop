f8203fea6f2409adcb2a58a53d353fc9891a5c17
Kopcho <rich@kopcho.com>
2025-07-21 22:57:54 -0600

    fix: moved index.html and updated config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Needed for Vercel root deployment
   server: {
     port: 5173,
-    strictPort: true,
-  },
-  build: {
-    outDir: 'dist'
+    strictPort: true
   }
 });
-
